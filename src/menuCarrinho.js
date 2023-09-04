2fdf5fd39b498264d740125e34b77f55eeb8aeec
João Cruz <jpedrosncruz@gmail.com>
2023-09-03 21:01:36 -0300

    Controlar itens do carrinho
    Agora foi adicionado a função que adiciona a quantidade de itens no carrinho e também que remove o card quando o item chega a 0

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,5 +1,7 @@
 import { catalogo } from "./utilidades";
 
+const idsProdutoCarrinhoComQuantidade = {};
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.add("right-[0px]");
     document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -18,20 +20,78 @@ export function inicializarCarrinho() {
     botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
 };
 
-export function adicionarAoCarrinho(idProduto) {
+function removerDoCarrinho(idProduto) {
+    delete idsProdutoCarrinhoComQuantidade[idProduto];
+    renderizarProdutosCarrinho();
+};
+
+function incrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atualizarInformacaoQuantidade(idProduto);
+};
+
+function decrementarQuantidadeProduto(idProduto) {
+    if (idsProdutoCarrinhoComQuantidade[idProduto] === 1){
+        removerDoCarrinho(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atualizarInformacaoQuantidade(idProduto);
+};
+
+function atualizarInformacaoQuantidade(idProduto){
+    document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+};
+
+function desenharProdutoNoCarrinho(idProduto) {
     const produto = catalogo.find((p) => p.id === idProduto);
     const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+    const elementoArticle = document.createElement("article");
+    const articleClasses = ['flex', 'bg-slate-200', 'rounded-lg', 'p-1', 'relative'];
+    
+    for (const articleClass of articleClasses) {
+        elementoArticle.classList.add(articleClass);
+    };
 
-    const cartaoProdutoCarrinho = `<article class="flex bg-slate-200 rounded-lg p-1 flex relative">
-    <button id="fechar-carrinho" class="absolute top-0 right-2">
+    const cartaoProdutoCarrinho = `<button id="remover-item-${produto.id}" class="absolute top-0 right-2">
       <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
     </button>
     <img class="h-24 rounded-lg" src="./assets/img/${produto.imagem}" alt="Carrinho: ${produto.nome}">
-    <div class="py-3">
-      <p class="text-slate-900 text-sm">${produto.nome}ã</p>
+    <div class="p-3 flex flex-col justify-between">
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
       <p class="text-slate-500 text-xs">Tamanho: M</p>
       <p class="text-green-700 text-lg">$${produto.preco}</p>
     </div>
-  </article>`;
-  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+    <div class="text-lg flex text-slate-900 items-end absolute bottom-0 right-2">
+        <button id="decrementar-produto-${produto.id}">-</button>
+        <p id="quantidade-${produto.id}" class="ml-2">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+        <button id="incrementar-produto-${produto.id}" class="ml-2">+</button>
+    </div>`;
+
+    elementoArticle.innerHTML = cartaoProdutoCarrinho;
+  containerProdutosCarrinho.appendChild(elementoArticle);
+
+  document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click', () => incrementarQuantidadeProduto(produto.id));
+  document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click', () => decrementarQuantidadeProduto(produto.id));
+  document.getElementById(`remover-item-${produto.id}`).addEventListener('click', () => removerDoCarrinho(produto.id));
+
+};
+
+function renderizarProdutosCarrinho() {
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+    containerProdutosCarrinho.innerHTML = "";
+
+    for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+        desenharProdutoNoCarrinho(idProduto);
+    };
+};
+
+export function adicionarAoCarrinho(idProduto) {
+    if(idProduto in idsProdutoCarrinhoComQuantidade){
+        incrementarQuantidadeProduto(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+    desenharProdutoNoCarrinho(idProduto);
+
 };
