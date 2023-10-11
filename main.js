8a2a0c958b26c19cfdf6d32fa234c0dd1e738c8c
João Cruz <jpedrosncruz@gmail.com>
2023-10-11 06:29:21 -0300

    getItens from localStorage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
-import { inicializarCarrinho } from "./src/menuCarrinho";
-
+import { inicializarCarrinho, renderizarProdutosCarrinho } from "./src/menuCarrinho";
 
 renderizarCatalogo();
 inicializarCarrinho();
+renderizarProdutosCarrinho();
