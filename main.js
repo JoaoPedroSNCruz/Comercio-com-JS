26cd6017f560bb61d48cfe3859761f8759ba9ff9
João Cruz <jpedrosncruz@gmail.com>
2023-10-11 06:56:22 -0300

    filter bar complete

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
 import { inicializarCarrinho, renderizarProdutosCarrinho } from "./src/menuCarrinho";
 
 renderizarCatalogo();
 inicializarCarrinho();
 renderizarProdutosCarrinho();
+inicializarFiltros();
--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -5,7 +5,7 @@ export function renderizarCatalogo() {
     for(const produtoCatalogo of catalogo) {
         
         const cartaoProduto = 
-        `<div class='group rounded-lg shadow-xl shadow-slate-400 m-2 w-48 flex flex-col p-2 justify-between' id="card-produto-${produtoCatalogo.id}">
+        `<div class='group rounded-lg shadow-xl shadow-slate-400 m-2 w-48 flex flex-col p-2 justify-between ${produtoCatalogo.feminino ? 'feminino' : 'masculino'}' id="card-produto-${produtoCatalogo.id}">
         <img class='group-hover:scale-110 duration-300 my-3 rounded-lg' src="./assets/img/${produtoCatalogo.imagem}" alt="Produto ${produtoCatalogo.id} do Comércio com JS">
         <p class="text-sm">${produtoCatalogo.marca}</p>
         <p class='text-sm'>${produtoCatalogo.nome}</p>
